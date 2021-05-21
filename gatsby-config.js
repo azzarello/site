a83d0185ca71e446fa55af64ca29d22e88c5b3d9
Connor Azzarello <connor.azzarello@gmail.com>
2021-05-21 11:47:05 -0700

    init commit, testing changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,35 +3,35 @@ module.exports = {
     // Site URL for when it goes live
     siteUrl: `https://elastic-meninsky-aa7c74.netlify.app/`,
     // Your Name
-    name: 'Ryan Fitzgerald',
+    name: 'Connor Azzarello',
     // Main Site Title
-    title: `Ryan Fitzgerald | Full-Stack Developer`,
+    title: `Connor Azzarello | Computer Science & Engineering Graduate`,
     // Description that goes under your name in main bio
     description: `Lorem ipsum dolor sit amet consectetur adipisicing elit, ipsum.`,
     // Optional: Twitter account handle
-    author: `@rfitzio`,
+    //author: `@rfitzio`,
     // Optional: Github account URL
-    github: `https://github.com/ryanfitzgerald`,
+    github: `https://github.com/azzarello`,
     // Optional: LinkedIn account URL
-    linkedin: `https://www.linkedin.com/in/ryanafitzgerald/`,
+    linkedin: `https://www.linkedin.com/in/connorazzarello/`,
     // Content of the About Me section
     about: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus perferendis porro cumque ea error ab voluptatem. Temporibus adipisci exercitationem similique itaque quibusdam laudantium, qui molestiae quas, aut amet animi id.`,
     // Optional: List your projects, they must have `name` and `description`. `link` is optional.
     projects: [
       {
-        name: 'Devfolio',
+        name: 'Gameday Guru',
         description:
           'A zero-config and blazing fast personal site + blog built with GatsbyJs and TailwindCSS',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
+        link: 'https://gamedayguru.com/',
       },
       {
-        name: 'ChromeExtensionKit',
+        name: 'Enhanced Sensing Methods for UAV-based Disaster Recovery',
         description:
           'Kit to jump-start your Chrome extension projects with a variety of battle-tested starter templates',
         link: 'https://chromeextensionkit.com/?ref=devfolio',
       },
       {
-        name: 'Another Cool Project',
+        name: 'Paper Prisons Project',
         description:
           'Lorem ipsum dolor sit amet consectetur adipisicing elit ducimus perferendis',
         link: 'https://github.com/RyanFitzgerald/devfolio',
@@ -40,18 +40,18 @@ module.exports = {
     // Optional: List your experience, they must have `name` and `description`. `link` is optional.
     experience: [
       {
-        name: 'Acme Corp',
-        description: 'Full-Stack Developer, February 2020 - Present',
+        name: 'Gameday Guru',
+        description: 'Head of Technology, December 2020 - Present',
         link: 'https://github.com/RyanFitzgerald/devfolio',
       },
       {
-        name: 'Globex Corp',
-        description: 'Full-Stack Developer, December 2017 - February 2020',
+        name: 'Samirian Chemicals',
+        description: 'Business Systems Analyst Intern, May 2020 - September 2020',
         link: 'https://github.com/RyanFitzgerald/devfolio',
       },
       {
         name: 'Hooli',
-        description: 'Full-Stack Developer, May 2015 - December 2017',
+        description: 'Data Science Intern, January 2019 - September 2019',
         link: 'https://github.com/RyanFitzgerald/devfolio',
       },
     ],
@@ -119,7 +119,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: `ADD YOUR TRACKING ID HERE`, // Optional Google Analytics
+        trackingId: `G-3NVWBHWFLR`, // Optional Google Analytics
       },
     },
     {
@@ -128,8 +128,8 @@ module.exports = {
         name: `devfolio`,
         short_name: `devfolio`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`, // This color appears on mobile
+        background_color: `#nord0`,
+        theme_color: `#nord0`, // This color appears on mobile
         display: `minimal-ui`,
         icon: `src/images/icon.png`,
       },
