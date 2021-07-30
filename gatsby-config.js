0174625fe2c7d33d0e476b05f998cc8040e16f18
Connor Azzarello <connor.azzarello@gmail.com>
2021-07-30 15:00:00 -0700

    updated most if not all the data from before

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,13 @@
 module.exports = {
   siteMetadata: {
     // Site URL for when it goes live
-    siteUrl: `https://elastic-meninsky-aa7c74.netlify.app/`,
+    siteUrl: `https://azzarello.codes`,
     // Your Name
     name: 'Connor Azzarello',
     // Main Site Title
     title: `Connor Azzarello | Computer Science & Engineering Graduate`,
     // Description that goes under your name in main bio
-    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit, ipsum.`,
+    description: `Passionate code-monkey looking for meaningful career opportunities`,
     // Optional: Twitter account handle
     //author: `@rfitzio`,
     // Optional: Github account URL
@@ -15,45 +15,45 @@ module.exports = {
     // Optional: LinkedIn account URL
     linkedin: `https://www.linkedin.com/in/connorazzarello/`,
     // Content of the About Me section
-    about: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus perferendis porro cumque ea error ab voluptatem. Temporibus adipisci exercitationem similique itaque quibusdam laudantium, qui molestiae quas, aut amet animi id.`,
+    about: `Recent Computer Science and Engineering graduate looking for full-time roles in the area of Dev-Ops, Data Engineer, and Software Engineering`,
     // Optional: List your projects, they must have `name` and `description`. `link` is optional.
     projects: [
       {
         name: 'Gameday Guru',
         description:
-          'A zero-config and blazing fast personal site + blog built with GatsbyJs and TailwindCSS',
+          'Side-project/early stage startup focusing on providing innovative sports data analytics using advanced machine learning and industry experience in the sports gaming industry.',
         link: 'https://gamedayguru.com/',
       },
-      // {
-      //   name: 'Enhanced Sensing Methods for UAV-based Disaster Recovery',
-      //   description:
-      //     'Kit to jump-start your Chrome extension projects with a variety of battle-tested starter templates',
-      //   link: 'https://chromeextensionkit.com/?ref=devfolio',
-      // },
-      // {
-      //   name: 'Paper Prisons Project',
-      //   description:
-      //     'Lorem ipsum dolor sit amet consectetur adipisicing elit ducimus perferendis',
-      //   link: 'https://github.com/RyanFitzgerald/devfolio',
-      // },
+      {
+        name: 'Enhanced Sensing Methods for UAV-based Disaster Recovery',
+        description 
+      'Developed a modular, low-cost UAV (drone) to locate injured or distressed victims in disaster scenarios. Implemented SSD-MobileNet v2 model using Docker for visible-light object-detection inference. Improved computational speed by 8.16% by re-training SSD-MobileNet to only detect the person object class which led to a 25.8% decrease in the model\'s overall memory footprint. Modified the NVIDIA Jetson Nano\'s scheduling algorithm to optimize battery life, achieving flight-time metrics on par with industry standard systems at 20% the cost.'
+        link: 'https://bit.ly/AzzarelloThesis',
+      },
+      {
+        name: 'Paper Prisons Initiative',
+        description:
+        'Analyzed correlations between criminal justice policies and outcomes on a national and state basis. Developed an interactive questionnaire to help recently released offenders navigate the process to obtain state identification, a requirement for job and housing applications, and facilitate reintegration into society. Designed a web application that allowed individuals to find out if they were eligible to have their criminal record expunged and provided specific steps they could pursue if they were allowed to apply.',
+        link: 'https://bit.ly/PaperPrisons',
+      },
     ],
     // Optional: List your experience, they must have `name` and `description`. `link` is optional.
     experience: [
       {
         name: 'Gameday Guru',
         description: 'Head of Technology, December 2020 - Present',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
+        link: 'https://gamedayguru.com',
       },
       {
         name: 'Samirian Chemicals',
         description:
           'Business Systems Analyst Intern, May 2020 - September 2020',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
+        link: 'http://samirian.com/',
       },
       {
-        name: 'Hooli',
+        name: 'Zoom Systems',
         description: 'Data Science Intern, January 2019 - September 2019',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
+        link: 'https://www.zoomsystems.com/',
       },
     ],
     // Optional: List your skills, they must have `name` and `description`.
@@ -61,7 +61,7 @@ module.exports = {
       {
         name: 'Languages & Frameworks',
         description:
-          'JavaScript (ES6+), Golang, Node.js, Express.js, React, Ruby on Rails, PHP',
+          'Python, C/C++, SQL, JavaScript, TypeScript, HTML/CSS, Golang, Java, R',
       },
       {
         name: 'Databases',
@@ -70,7 +70,7 @@ module.exports = {
       {
         name: 'Other',
         description:
-          'Docker, Amazon Web Services (AWS), CI / CD, Microservices, API design, Agile / Scrum',
+          'Docker, Google Cloud Platform (GCP), CI / CD, Jenkins, Linux, TravisCI, Heroku'
       },
     ],
   },
